ff264b53ee6851f5d6319d3006808c8f1b083f50
EnrYu0501 <dolehuan260@gmail.com>
2022-04-20 16:56:42 +0700

    update source core

---
+++ webtruyen/Areas/Admin/Controllers/TheLoaiController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using webtruyen.Models;
+
+namespace webtruyen.Areas.Admin.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        // GET: Admin/TheLoai
+        private DB myDb = new DB();
+        public ActionResult ds_TL()
+        {
+            if (Session["loai"] == null)
+            {
+                return RedirectToAction("TrangChu", "trangChu", new { area = "" });
+            }
+            else
+            {
+                List<THELOAI> listTL = myDb.THELOAIs.ToList();
+                return View(listTL);
+            }
+
+           
+        }
+
+        public ActionResult Create()
+        {
+            if (Session["loai"] == null)
+            {
+                return RedirectToAction("TrangChu", "trangChu", new { area = "" });
+            }
+            else
+            {
+                if (Request.Form.Count > 0)
+                {
+
+                    string tenTL = Request.Form["TENTHELOAI"];
+                    string mota = Request.Form["MOTA"];
+                    THELOAI TL = new THELOAI();
+
+                    TL.TENTHELOAI = tenTL;
+                    TL.MOTA = mota;
+                    myDb.THELOAIs.Add(TL);
+                    myDb.SaveChanges();
+                    return RedirectToAction("ds_TL");
+
+                }
+                return View();
+            }
+
+            
+        }
+        public ActionResult Edit(int maTL)
+        {
+            if (Session["loai"] == null)
+            {
+                return RedirectToAction("TrangChu", "trangChu", new { area = "" });
+            }
+            else
+            {
+                THELOAI TL = myDb.THELOAIs.FirstOrDefault(p => p.MATHELOAI == maTL);
+                if (Request.Form.Count > 0)
+                {
+                    string tenTL = Request.Form["TENTHELOAI"];
+                    string mota = Request.Form["MOTA"];
+                    TL.TENTHELOAI = tenTL;
+                    TL.MOTA = mota;
+                    myDb.SaveChanges();
+                    return RedirectToAction("ds_TL");
+                }
+                return View(TL);
+            }
+
+            
+        }
+        public ActionResult Delete(int maTL)
+        {
+            THELOAI TL = myDb.THELOAIs.FirstOrDefault(p => p.MATHELOAI == maTL);
+            if (TL != null)
+            {
+                myDb.THELOAIs.Remove(TL);
+                myDb.SaveChanges();
+            }
+            return RedirectToAction("ds_TL");
+        }
+    }
+}
---
+++ webtruyen/webtruyen/Areas/Admin/Controllers/LoaiTKController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using webtruyen.Models;
+
+namespace webtruyen.Areas.Admin.Controllers
+{
+    public class LoaiTKController : Controller
+    {
+        // GET: Admin/LoaiTK
+
+        private DB myDb = new DB();
+        public ActionResult ds_loaiTK()
+        {
+            if (Session["loai"] == null)
+            {
+                return RedirectToAction("TrangChu", "trangChu", new { area = "" });
+            }
+            else
+            {
+                List<LOAITAIKHOAN> listLoaiTK = myDb.LOAITAIKHOANs.ToList();
+                return View(listLoaiTK);
+            }
+
+            
+        }
+
+        public ActionResult Create()
+        {
+            if (Session["loai"] == null)
+            {
+                return RedirectToAction("TrangChu", "trangChu", new { area = "" });
+            }
+            else
+            {
+                if (Request.Form.Count > 0)
+                {
+
+                    string tenloaiTK = Request.Form["TENLOAITAIKHOAN"];
+
+                    LOAITAIKHOAN loaiTK = new LOAITAIKHOAN();
+
+                    loaiTK.TENLOAITAIKHOAN = tenloaiTK;
+                    myDb.LOAITAIKHOANs.Add(loaiTK);
+                    myDb.SaveChanges();
+                    return RedirectToAction("ds_loaiTK");
+
+                }
+                return View();
+            }
+
+          
+        }
+        public ActionResult Edit(int maLoaiTK)
+        {
+            if (Session["loai"] == null)
+            {
+                return RedirectToAction("TrangChu", "trangChu", new { area = "" });
+            }
+            else
+            {
+                LOAITAIKHOAN loaiTK = myDb.LOAITAIKHOANs.FirstOrDefault(p => p.MALOAITK == maLoaiTK);
+                if (Request.Form.Count > 0)
+                {
+                    string tenLoaiTK = Request.Form["TENLOAITAIKHOAN"];
+                    loaiTK.TENLOAITAIKHOAN = tenLoaiTK;
+                    myDb.SaveChanges();
+                    return RedirectToAction("ds_loaiTK");
+                }
+                return View(loaiTK);
+            }
+
+           
+        }
+        public ActionResult Delete(int maLoaiTK)
+        {
+            LOAITAIKHOAN loaiTK = myDb.LOAITAIKHOANs.FirstOrDefault(p => p.MALOAITK == maLoaiTK);
+            if (loaiTK != null)
+            {
+                myDb.LOAITAIKHOANs.Remove(loaiTK);
+                myDb.SaveChanges();
+            }
+            return RedirectToAction("ds_loaiTK");
+        }
+    }
+}
